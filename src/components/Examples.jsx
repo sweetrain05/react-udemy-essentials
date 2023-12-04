3fa8e41474e5efa05912a910e7b77da797d840c8
Danbi Choi <sweetrain05@hotmail.com>
2023-12-04 14:29:34 -0800

    Forwarding props to wrapped elements using rest property

--- src/components/Examples.jsx
+++ src/components/Examples.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 
 import TabButton from './TabButton';
 import { EXAMPLES } from '../data.js';
+import Section from './Section';
 
 const Examples = () => {
     const [selectedTopic, setSelectedTopic] = useState();
@@ -26,36 +27,35 @@ const Examples = () => {
     }
 
     return (
-        <section id='examples'>
-            <h2>Examples</h2>
+        <Section title='Examples' id='examples'>
             <menu>
                 <TabButton
                     isSelected={selectedTopic === 'components'}
-                    onSelect={() => handleSelect('components')}
+                    onClick={() => handleSelect('components')}
                 >
                     Components
                 </TabButton>
                 <TabButton
                     isSelected={selectedTopic === 'jsx'}
-                    onSelect={() => handleSelect('jsx')}
+                    onClick={() => handleSelect('jsx')}
                 >
                     JSX
                 </TabButton>
                 <TabButton
                     isSelected={selectedTopic === 'props'}
-                    onSelect={() => handleSelect('props')}
+                    onClick={() => handleSelect('props')}
                 >
                     Props
                 </TabButton>
                 <TabButton
                     isSelected={selectedTopic === 'state'}
-                    onSelect={() => handleSelect('state')}
+                    onClick={() => handleSelect('state')}
                 >
                     State
                 </TabButton>
             </menu>
             {tabContent}
-        </section>
+        </Section>
     );
 };
 export default Examples;
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,10 +1,10 @@
-export default function TabButton({ children, onSelect, isSelected }) {
-  console.log('TABBUTTON COMPONENT EXECUTING');
-  return (
-    <li>
-      <button className={isSelected ? 'active' : undefined} onClick={onSelect}>
-        {children}
-      </button>
-    </li>
-  );
+export default function TabButton({ children, isSelected, ...props }) {
+    console.log('TABBUTTON COMPONENT EXECUTING');
+    return (
+        <li>
+            <button className={isSelected ? 'active' : undefined} {...props}>
+                {children}
+            </button>
+        </li>
+    );
 }
